994a73e0fe0e743bfc5a6d4ff2b52da87c46ce7f
saleksandra <soltys.ola@gmail.com>
2020-01-08 07:41:50 +0100

    SELV3-32: Initial commit for equipment-ui service

---
+++ jenkins.sh
@@ -0,0 +1,17 @@
+echo "building image"
+cp ./credentials/test_env/settings.env .env
+
+/usr/local/bin/docker-compose pull
+/usr/local/bin/docker-compose down --volumes
+/usr/local/bin/docker-compose run --entrypoint ./build.sh equipment-ui
+/usr/local/bin/docker-compose build image
+/usr/local/bin/docker-compose down --volumes
+
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+
+echo "pushing image for dev/test instance"
+docker tag openlmismz/equipment-ui:latest openlmismz/equipment-ui:${version}
+docker push openlmismz/equipment-ui:${version}
+
+rm -Rf ./credentials
+rm -f .env
